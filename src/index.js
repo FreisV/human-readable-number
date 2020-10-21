e7ebfc2094bf9dd6ae4b54016c44764129bbe92a
FreisV <Ichvadikz@gmail.com>
2020-10-22 01:40:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,99 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let dozen;
+    let hundred;
+
+    const numbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+
+    const dozens = [
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const arr10to19 = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    if (number < 100) {
+			dozen = Math.floor(number / 10)
+        if (number < 10) {
+            return numbers[number];
+        } else if (number < 20) {
+            return arr10to19[number % 10];
+        } else {
+            if (number % 10 == 0) {
+                return dozens[dozen - 1];
+            } else {
+                return dozens[dozen - 1] + " " + numbers[number % 10];
+            }
+        }
+    } else if (number < 1000) {
+				hundred = Math.floor(number / 100);
+				dozen = Math.floor(number / 10 % 10);
+				console.log(`hundred: ${hundred}`);
+				console.log(`dozen: ${dozen}`)
+
+        if (dozen == 0) {
+            if (number % 10 == 0) {
+                return numbers[hundred] + " " + "hundred";
+            } else {
+                return (
+                    numbers[hundred] +
+                    " " +
+                    "hundred " +
+                    numbers[number % 10]
+                );
+            }
+        } else if (dozen == 1) {
+            return (
+                numbers[hundred] + " " + "hundred " + arr10to19[number % 10]
+            );
+        } else if (dozen != 0) {
+            if (number % 10 == 0) {
+                return (
+                    numbers[hundred] +
+                    " " +
+                    "hundred " +
+                    dozens[dozen - 1]
+                );
+            } else {
+                return (
+                    numbers[hundred] +
+                    " " +
+                    "hundred " +
+                    dozens[dozen - 1] +
+                    " " +
+                    numbers[number % 10]
+                );
+            }
+        }
+    } 
 }
+
+//console.log(toReadable(999));
